6ec1ecc2e986b457d462e75b83fa780852b2a4b2
Olga Zorina <olgzorina@yandex.ru>
2022-07-31 13:56:47 +0300

    Create TodoItem struct

---
+++ TodoList/TodoList/Models/TodoItem.swift
@@ -0,0 +1,35 @@
+//
+//  TodoItem.swift
+//  TodoList
+//
+//  Created by Olga Zorina on 7/31/22.
+//  Copyright © 2022 Olga Zorina. All rights reserved.
+//
+
+import Foundation
+
+enum Importance: String {
+    case high = "high"
+    case medium = "medium"
+    case low = "low"
+}
+
+struct TodoItem {
+    let id: String
+    let text: String
+    let importance: Importance
+    let deadline: Date?
+    let isDone: Bool
+    let createdAt: Date
+    let modifiedAt: Date?
+
+    init(id: String = UUID().uuidString, text: String, importance: Importance, deadline: Date? = nil, isDone: Bool, createdAt: Date, modifiedAt: Date? = nil) {
+        self.id = id
+        self.text = text
+        self.importance = importance
+        self.deadline = deadline
+        self.isDone = isDone
+        self.createdAt = createdAt
+        self.modifiedAt = modifiedAt
+    }
+}
