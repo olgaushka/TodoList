9b8511fa6dadbdb8a728292bef3c6eacdd92b2af
Olga Zorina <olgzorina@yandex.ru>
2022-08-04 21:23:45 +0300

    Added TodoItemsListScreen and renamed some folders

---
+++ TodoList/TodoList/TodoItemsListScreen/TodoItemsListView.swift
@@ -0,0 +1,9 @@
+//
+//  TodoItemsListView.swift
+//  TodoList
+//
+//  Created by Olga Zorina on 8/4/22.
+//  Copyright © 2022 Olga Zorina. All rights reserved.
+//
+
+import Foundation
---
+++ TodoList/TodoList/TodoItemsListScreen/TodoItemsListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TodoItemsListViewController.swift
+//  TodoList
+//
+//  Created by Olga Zorina on 8/4/22.
+//  Copyright © 2022 Olga Zorina. All rights reserved.
+//
+
+import Foundation
