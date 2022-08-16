2435d8ab51c309d65b04c0ba450718cea305a8aa
Olga Zorina <olgzorina@yandex.ru>
2022-08-17 00:36:03 +0300

    Add NetworkTodoItem

---
+++ TodoList/TodoList/Models/NetworkTodoItem.swift
@@ -0,0 +1,73 @@
+//
+//  NetworkTodoItem.swift
+//  TodoList
+//
+//  Created by Olga Zorina on 8/16/22.
+//  Copyright © 2022 Olga Zorina. All rights reserved.
+//
+
+import Foundation
+import TodoListModels
+
+enum NetworkImportance: String, Codable {
+    case high = "important"
+    case medium = "basic"
+    case low = "low"
+}
+
+struct NetworkTodoItem: Codable {
+    let id: String
+    let text: String
+    let importance: NetworkImportance
+    let deadline: Int64?
+    let done: Bool
+    let createdAt: Int64
+    let changedAt: Int64?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case text
+        case importance
+        case deadline
+        case done
+        case createdAt = "created_at"
+        case changedAt = "changed_at"
+    }
+
+    init(item: TodoItem) {
+        self.id = item.id
+        self.text = item.text
+        switch item.importance {
+        case .low:
+            self.importance = .low
+        case .medium:
+            self.importance = .medium
+        case .high:
+            self.importance = .high
+        }
+        if let deadline = item.deadline?.timeIntervalSince1970 {
+            self.deadline = Int64(deadline)
+        } else {
+            self.deadline = nil
+        }
+        self.done = item.isDone
+        self.createdAt = Int64(item.createdAt.timeIntervalSince1970)
+        if let changedAt = item.modifiedAt?.timeIntervalSince1970 {
+            self.changedAt = Int64(changedAt)
+        } else {
+            self.changedAt = nil
+        }
+    }
+}
+
+// {
+//  "id": <uuid>, // уникальный идентификатор элемента
+//  "text": "blablabla",
+//  "importance": "<importance>", // importance = low | basic | important
+//  "deadline": <unix timestamp>, // int64, может отсутствовать, тогда нет
+//  "done": <bool>,
+//  "color": "#FFFFFF", // может отсутствовать
+//  "created_at": <unix timestamp>,
+//  "changed_at": <unix timestamp>,
+//  "last_updated_by": <device id>
+// }
